c516b42a0c0a004bcfd10b778f8b345eeedd6632
Charanvir <charanvir123@gmail.com>
2022-09-01 00:04:57 -0400

    added function to submit cart and user data to firebase

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -48,6 +48,13 @@ const Checkout = (props) => {
 
             return;
         }
+
+        props.onConfirm({
+            name: enteredName,
+            street: enteredStreet,
+            postalCode: enteredPostal,
+            city: enteredCity
+        })
     };
 
     return (
